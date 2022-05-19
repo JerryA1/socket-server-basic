0cf145170547979a224c736bc98b8105a699f94a
JerryA1 <gera_al.v@hotmail.com>
2022-05-19 17:11:41 -0500

    first commit

---
+++ models/server.js
@@ -0,0 +1,40 @@
+// Servidor de express
+const express = require('express');
+const http = require('http')
+const socketio = require('socket.io');
+const path = require('path');
+const Sockets = require('./sockets');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Htpp server
+        this.server = http.createServer(this.app);
+
+        // Configuraciones de sockets
+        this.io = socketio(this.server, { /** Configuaraciones */ });
+    }
+
+    middlewares() {
+        // Desplegar directorio publico
+        this.app.use(express.static(path.resolve(__dirname, '../public')));
+    }
+
+    socketsConfiguration() { 
+        new Sockets(this.io);
+    }
+
+    execute() {
+        this.middlewares();
+
+        this.socketsConfiguration();
+
+        this.server.listen(this.port, () => {
+            console.log('🚀 Server on port:', this.port);
+        });
+    }
+}
+
+module.exports = Server;
