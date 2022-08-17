3484f815aa1ed711ef91075401f25d27f3bb5a9d
JerryA1 <gera_al.v@hotmail.com>
2022-08-17 15:44:14 -0500

    ♻️ REFACTOR: Refactorizaci[on de modelos

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http')
 const socketio = require('socket.io');
 const path = require('path');
+const cors = require('cors');
 const Sockets = require('./sockets');
 
 class Server {
@@ -20,6 +21,8 @@ class Server {
     middlewares() {
         // Desplegar directorio publico
         this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+        this.app.use(cors());
     }
 
     socketsConfiguration() { 
